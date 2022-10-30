5f1f8b0df2c20fbacd92079d23a918fbe5f79560
AlphaP45 <alphapramadhani45@gmail.com>
2022-10-30 21:24:40 +0700

    Membuat View Contact

---
+++ resources/views/Contact.blade.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+        <title>BJM Bengkel Mobil</title>
+        <meta content="" name="description">
+        <meta content="" name="keywords">
+
+        <!-- Favicons -->
+        <link href="assets/img/favicon.png" rel="icon">
+        <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+        <!-- Google Fonts -->
+        <link
+            href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+            rel="stylesheet">
+
+        <!-- Vendor CSS Files -->
+        <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+        <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+        <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+        <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+        <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+        <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+        <!-- Template Main CSS File -->
+        <link href="assets/css/style.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <!-- ======= Header ======= -->
+        <header id="header" class="fixed-top ">
+            <div class="container d-flex align-items-center">
+
+                <h1 class="logo me-auto"><a href="index.html">BJM</a></h1>
+
+                <nav id="navbar" class="navbar">
+                    <ul>
+                        <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                        <li><a class="nav-link scrollto" href="#about">About</a></li>
+                        <li><a class="nav-link scrollto" href="#services">Contact</a></li>
+
+                        <li><a class="getstarted scrollto" href="#about">Log In</a></li>
+                    </ul>
+                    <i class="bi bi-list mobile-nav-toggle"></i>
+                </nav><!-- .navbar -->
+
+            </div>
+        </header><!-- End Header -->
+
+        <!-- ======= Contact Section ======= -->
+                <section id="contact" class="contact">
+                    <div class="container" data-aos="fade-up">
+
+                      <div class="section-title">
+                        <h2>Contact</h2>
+                        <p>Bila anda kesulitan untuk datang ketempat kami silahkan hubungi nomor yang telah disediakan diform contact berikut</p>
+                      </div>
+
+                      <div class="row">
+
+                        <div class="col-lg-5 d-flex align-items-stretch">
+                          <div class="info">
+                            <div class="address">
+                              <i class="bi bi-geo-alt"></i>
+                              <h4>Lokasi:</h4>
+                              <p>Kota Sidoarjo</p>
+                            </div>
+
+                            <div class="email">
+                              <i class="bi bi-envelope"></i>
+                              <h4>Email:</h4>
+                              <p>BJMSidoarjo@gmail.com</p>
+                            </div>
+
+                            <div class="phone">
+                              <i class="bi bi-phone"></i>
+                              <h4>Telephon:</h4>
+                              <p>088123456</p>
+                            </div>
+
+                            <iframe src="https://goo.gl/maps/kg1RhJeXyBHtgUiQ7" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+                          </div>
+
+                        </div>
+
+                        <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+                          <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                            <div class="row">
+                              <div class="form-group col-md-6">
+                                <label for="name">Masukkan Nama</label>
+                                <input type="text" name="name" class="form-control" id="name" required>
+                              </div>
+                              <div class="form-group col-md-6">
+                                <label for="name">Masukkan Email</label>
+                                <input type="email" class="form-control" name="email" id="email" required>
+                              </div>
+                            </div>
+                            <div class="form-group">
+                              <label for="name">Subject</label>
+                              <input type="text" class="form-control" name="subject" id="subject" required>
+                            </div>
+                            <div class="form-group">
+                              <label for="name">Pesan</label>
+                              <textarea class="form-control" name="message" rows="10" required></textarea>
+                            </div>
+                            <div class="my-3">
+                              <div class="loading">Loading</div>
+                              <div class="error-message"></div>
+                              <div class="sent-message">Pesanmu telah terkirim. Terima Kasih</div>
+                            </div>
+                            <div class="text-center"><button type="submit">Send Message</button></div>
+                          </form>
+                        </div>
+
+                      </div>
+
+                    </div>
+                  </section><!-- End Contact Section -->
+                </div>
+                <!-- ======= Footer ======= -->
+                <footer id="footer">
+
+                    <div class="footer-newsletter">
+                        <div class="container">
+                            <div class="row justify-content-center">
+                                <div class="col-lg-6">
+                                    <h4>Bengkel Online Mobil</h4>
+                                    <p>Percayakan Dokter Mobil untuk atasi berbagai permasalahan mobil Anda, dari</p>
+                                    <p>perawatan AC mobil hingga memaksimalkan perfoma mobil</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="footer-top">
+                        <div class="container">
+                            <div class="row">
+
+                                <div class="col-lg-3 col-md-6 footer-contact">
+                                    <h3>BJM</h3>
+                                    <p>Bengkel Mobil Sidoarjo <br>&copy; 2022 <strong><span>BJM</span></strong>. All Rights Reserved</p>
+                                </div>
+
+                                <div class="col-lg-2 col-md-6 footer-links">
+                                    <h4>Link</h4>
+                                    <ul>
+                                        <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                                        <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+                                        <li><i class="bx bx-chevron-right"></i> <a href="#">Contact</a></li>
+                                        <li><i class="bx bx-chevron-right"></i> <a href="#">Login</a></li>
+                                        <li><i class="bx bx-chevron-right"></i> <a href="#">Register</a></li>
+                                    </ul>
+                                </div>
+
+                                <div class="col-lg-3 col-md-6 footer-links">
+                                    <h4>Layanan Kami</h4>
+                                    <ul>
+                                        <li><i class="bx bx-chevron-right"></i> <a href="#">Penyedia Service Panggilan</a></li>
+                                        <li><i class="bx bx-chevron-right"></i> <a href="#">Diagnosa Kerusakan Mobil</a></li>
+                                        <li><i class="bx bx-chevron-right"></i> <a href="#">Sistem Sewa</a></li>
+                                        <li><i class="bx bx-chevron-right"></i> <a href="#">Service Mobil</a></li>
+                                        {{-- <li><i class="bx bx-chevron-right"></i> <a href="#"></a></li> --}}
+                                    </ul>
+                                </div>
+
+                                <div class="col-lg-4 col-md-6 footer-links">
+                                    <h4>IKUTI KAMI</h4>
+                                    <p>BJM mempunyai beberapa layanan Sosial Media yang bisa Anda kunjungi</p>
+                                    <div class="social-links mt-3">
+                                        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>Instagram
+                                        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>Facebook
+                                        <a href="#" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>Whatsapp
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="container footer-bottom clearfix">
+                        <div class="copyright">
+                            &copy; Copyright <strong><span>Arsha</span></strong>. All Rights Reserved
+                        </div>
+                        <div class="credits">
+                            <!-- All the links in the footer should remain intact. -->
+                            <!-- You can delete the links only if you purchased the pro version. -->
+                            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
+                            Designed by <a href="">Kelompok 6 - TI 3E</a>
+                        </div>
+                    </div>
+                </footer><!-- End Footer -->
+                <!-- Vendor JS Files -->
+                <script src="assets/vendor/aos/aos.js"></script>
+                <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+                <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+                <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+                <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+                <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+                <script src="assets/vendor/php-email-form/validate.js"></script>
+
+                <!-- Template Main JS File -->
+                <script src="assets/js/main.js"></script>
+
+    </body>
+
+    </html>
