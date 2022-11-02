4395699f307972f155c4d23ed0de81cabc31fbe7
AlphaP45 <alphapramadhani45@gmail.com>
2022-11-02 09:46:52 +0700

    Update View Contact

--- resources/views/Contact.blade.php
+++ resources/views/Contact.blade.php
@@ -74,7 +74,7 @@
 
               <div class="section-title">
                 <h2>Contact</h2>
-                <p>Bila anda kesulitan untuk datang ketempat kami silahkan hubungi nomor yang telah disediakan diform contact berikut</p>
+                <p>Anda dapat menghubungi/menemukan kami di: </p>
               </div>
 
               <div class="row">
