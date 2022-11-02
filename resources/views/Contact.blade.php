e34e09213f9aa028c016edf5a1aca5a3038064b3
AlphaP45 <alphapramadhani45@gmail.com>
2022-11-02 09:10:11 +0700

    Update View Contact

--- resources/views/Contact.blade.php
+++ resources/views/Contact.blade.php
@@ -45,7 +45,7 @@
                 <li><a class="nav-link scrollto" href="/AboutUs">About</a></li>
                 <li><a class="nav-link scrollto active" href="/Contact">Contact</a></li>
                 <li><a class="getstarted scrollto" href="login">Log In</a></li>
-                @else 
+                @else
                 <li><a class="nav-link scrollto" href="/">Home</a></li>
                 <li><a class="nav-link scrollto" href="/AboutUs">About</a></li>
                 <li><a class="nav-link scrollto active" href="/Contact">Contact</a></li>
@@ -53,8 +53,8 @@
                 <li>
                     <a class="nav-link scrollto" href="{{ route('logout') }}"
                         onclick="event.preventDefault();
-                                                                                               document.getElementById('logout-form').submit();">
-                         Log Out    
+                        document.getElementById('logout-form').submit();">
+                        Log Out
                     </a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                         @csrf
@@ -68,78 +68,77 @@
     </div>
 </header><!-- End Header -->
 
-
-
         <!-- ======= Contact Section ======= -->
-                <section id="contact" class="contact">
-                    <div class="container" data-aos="fade-up">
+        <section id="contact" class="contact">
+            <div class="container" data-aos="fade-up">
 
-                      <div class="section-title">
-                        <h2>Contact</h2>
-                        <p>Bila anda kesulitan untuk datang ketempat kami silahkan hubungi nomor yang telah disediakan diform contact berikut</p>
-                      </div>
+              <div class="section-title">
+                <h2>Contact</h2>
+                <p>Bila anda kesulitan untuk datang ketempat kami silahkan hubungi nomor yang telah disediakan diform contact berikut</p>
+              </div>
 
-                      <div class="row">
+              <div class="row">
 
-                        <div class="col-lg-5 d-flex align-items-stretch">
-                          <div class="info">
-                            <div class="address">
-                              <i class="bi bi-geo-alt"></i>
-                              <h4>Lokasi:</h4>
-                              <p><a target = "_blank" href="https://goo.gl/maps/75T63qDFUe1XZoEp9">Jl. Raya Kesambi No.3, Dusun Pojok, Lajuk, Kec. Porong, Kabupaten Sidoarjo, Jawa Timur 61274</a></p>
-                            </div>
-
-                            <div class="email">
-                              <i class="bi bi-envelope"></i>
-                              <h4>Email:</h4>
-                              <p><a href = "mailto:alphapramadhani@gmail.com?">BJMSidoarjo@gmail.com</p>
-                            </div>
-
-                            <div class="phone">
-                              <i class="bi bi-phone"></i>
-                              <h4>Telepon:</h4>
-                              <p><a target= " _blank " href="https://api.whatsapp.com/send?phone=6285707569129&text=Saya%20ingin%20menyervis%20mobil%20saya%20di%20bengkel%20anda">085707569129</a></p>
-                            </div>
-
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.459031940696!2d112.67046461477642!3d-7.52480609457274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7df2c41973f91%3A0x4a3239af14dfa6c2!2sBENGKEL%20MOBIL%20BJM%20PORONG!5e0!3m2!1sid!2sid!4v1667287859673!5m2!1sid!2sid"  frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
-                          </div>
-
-                        </div>
-
-                        <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
-                          <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-                            <div class="row">
-                              <div class="form-group col-md-6">
-                                <label for="name">Masukkan Nama</label>
-                                <input type="text" name="name" class="form-control" id="name" required>
-                              </div>
-                              <div class="form-group col-md-6">
-                                <label for="name">Masukkan Email</label>
-                                <input type="email" class="form-control" name="email" id="email" required>
-                              </div>
-                            </div>
-                            <div class="form-group">
-                              <label for="name">Subject</label>
-                              <input type="text" class="form-control" name="subject" id="subject" required>
-                            </div>
-                            <div class="form-group">
-                              <label for="name">Pesan</label>
-                              <textarea class="form-control" name="message" rows="10" required></textarea>
-                            </div>
-                            <div class="my-3">
-                              <div class="loading">Loading</div>
-                              <div class="error-message"></div>
-                              <div class="sent-message">Pesanmu telah terkirim. Terima Kasih</div>
-                            </div>
-                            <div class="text-center"><button type="submit">Send Message</button></div>
-                          </form>
-                        </div>
-
-                      </div>
-
+                <div class="col-lg-5 d-flex align-items-stretch">
+                  <div class="info">
+                    <div class="address">
+                      <i class="bi bi-geo-alt"></i>
+                      <h4>Lokasi:</h4>
+                      <p><a target = "_blank" href="https://goo.gl/maps/75T63qDFUe1XZoEp9">Jl. Raya Kesambi No.3, Dusun Pojok, Lajuk, Kec. Porong, Kabupaten Sidoarjo, Jawa Timur 61274</a></p>
                     </div>
-                  </section><!-- End Contact Section -->
+
+                    <div class="email">
+                      <i class="bi bi-envelope"></i>
+                      <h4>Email:</h4>
+                      <p><a href = "mailto:alphapramadhani@gmail.com?">BJMSidoarjo@gmail.com</p>
+                    </div>
+
+                    <div class="phone">
+                      <i class="bi bi-phone"></i>
+                      <h4>Telepon:</h4>
+                      <p><a target= " _blank " href="https://api.whatsapp.com/send?phone=6285707569129&text=Saya%20ingin%20menyervis%20mobil%20saya%20di%20bengkel%20anda">085707569129</a></p>
+                    </div>
+
+                  </div>
+
                 </div>
+
+                <div class="col-lg-7 mt-10 mt-lg-0 d-flex align-items-stretch">
+                  <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                    <div class="row">
+                      <div class="form-group col-md-6">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.459031940696!2d112.67046461477642!3d-7.52480609457274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7df2c41973f91%3A0x4a3239af14dfa6c2!2sBENGKEL%20MOBIL%20BJM%20PORONG!5e0!3m2!1sid!2sid!4v1667287859673!5m2!1sid!2sid"  frameborder="0" style="border:0; width: 208%; height: 400px;" allowfullscreen></iframe>
+                      </div>
+                        {{-- <label for="name">Masukkan Nama</label>
+                        <input type="text" name="name" class="form-control" id="name" required>
+                      </div>
+                      <div class="form-group col-md-6">
+                        <label for="name">Masukkan Email</label>
+                        <input type="email" class="form-control" name="email" id="email" required>
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="name">Subject</label>
+                      <input type="text" class="form-control" name="subject" id="subject" required>
+                    </div>
+                    <div class="form-group">
+                      <label for="name">Pesan</label>
+                      <textarea class="form-control" name="message" rows="10" required></textarea>
+                    </div>
+                    <div class="my-3">
+                      <div class="loading">Loading</div>
+                      <div class="error-message"></div>
+                      <div class="sent-message">Pesanmu telah terkirim. Terima Kasih</div>
+                    </div>
+                    <div class="text-center"><button type="submit">Send Message</button></div> --}}
+                  </form>
+                </div>
+
+              </div>
+
+            </div>
+          </section><!-- End Contact Section -->
+        </div>
                <!-- ======= Footer ======= -->
             <footer id="footer">
 
